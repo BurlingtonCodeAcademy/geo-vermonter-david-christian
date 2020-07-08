5f9a6d179f6b423fd4fafd0c89b39271e2206367
802ScubaFish <christianrdow@gmail.com>
2020-07-08 15:38:37 -0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import VTMap from './VTMap.js';
 
 ReactDOM.render(
-  <h1>Hello World!</h1>,
+  <div>
+    <h1>Hello World!</h1>
+  <div style={{ height: '600px', width: '600px' }}>
+      <VTMap />
+    </div>
+  </div>,
   document.getElementById('root')
 )
